b53827e1197fd6cae9a2497d45679b054572043f
hakan.kuru <hakan.kuru@etiya.com>
2025-02-18 10:55:22 +0300

    fixedupdatedv6

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,15 @@ export default function App() {
             <div className="font-sans bg-gray-100 min-h-screen">
                 {/* Navbar */}
                 <nav className="bg-blue-900 text-white p-4 flex justify-between items-center shadow-md">
-                    <h1 className="text-2xl font-bold">Çorlu Otomatik Kapı</h1>
+                    <a href="/"
+                       className="flex items-center justify-center p-1 border-2 border-gray-400 rounded-md shadow-sm hover:shadow-md transition duration-300">
+                        <h1 className="bg-white p-1 text-lg font-bold text-gray-800 transition duration-300 hover:text-gray-600">
+                            <span className="text-blue-500">Çorlu</span><span className="text-red-500">OtomatikKapı</span>
+                            <div style={{fontSize:'13px',letterSpacing: '3px',textAlign: 'center',color: '#535353',fontStyle: 'italic'}}>Geçiş Sistemleri</div>
+                        </h1>
+                    </a>
                     <button className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
-                        <Menu size={28}/>
+                        <Menu size={40}/>
                     </button>
                     <ul className={`md:flex gap-6 ${menuOpen ? "block absolute top-16 left-0 w-full bg-blue-900 p-4 shadow-md" : "hidden md:flex"}`}>
                         {['Anasayfa', 'Kurumsal', 'Ürünlerimiz', 'Referanslar', 'İletişim'].map((item, indx) => (
@@ -28,10 +34,11 @@ export default function App() {
                         ))}
                     </ul>
                 </nav>
-                <div className="garage-door-info shadow2">
-                    <div className="garage-door-info-sub shadow2">
+                <div className="garage-door-info shadow2 bgSizeClass" style={{backgroundImage: 'url(img/images.jpg)'}}>
+                    <div className="p-6 max-sm:p-0 garage-door-info-sub bgblur shadow2">
                         <BusinessCard/>
-                    <p>
+
+                        <p className="text-white cursor-pointer p-1 sub hover:scale-105 transition duration-300">
                         Alüminyum ve çelik profillerden üretilen otomatik kapılar, dış cephenize uygun renk seçenekleri
                         ile estetiği,
                         sessiz çalışma ve uzaktan kumanda ile kullanım özelliği ile konforu, dış etkilere karşı yüksek
@@ -39,21 +46,22 @@ export default function App() {
                         güvenliği sizlere bir arada sunmaktadır.
                     </p>
 
-                    <p>
-                        Elektrik kesintisi durumunda da manuel olarak kapınızı açıp-kapatabilir, dilerseniz kesintisiz
-                        güç kaynağı
-                        kullanarak da kapınızı otomatik olarak çalıştırabilir, uzaktan kumanda kullanımı yanında,
-                        anahtar veya butonla
-                        da kapınızı kontrol edebilirsiniz.
-                    </p>
+                        <p className="text-white cursor-pointer p-1 sub hover:scale-105 transition duration-300">
+                            Elektrik kesintisi durumunda da manuel olarak kapınızı açıp-kapatabilir, dilerseniz
+                            kesintisiz
+                            güç kaynağı
+                            kullanarak da kapınızı otomatik olarak çalıştırabilir, uzaktan kumanda kullanımı yanında,
+                            anahtar veya butonla
+                            da kapınızı kontrol edebilirsiniz.
+                        </p>
 
-                    <p>
-                        Garajınızı ya da işyerinizi, her türlü hava şartlarından, hırsızlık ve zarar verme amaçlı
-                        yapılan fiziksel
-                        etkilerden korumak için, üstün güvenlik sistemiyle donatılmış, uzun ömürlü, rollmina Garaj
-                        Kapısı Sistemlerini
-                        güvenle binalarınıza uygulayabilirsiniz.
-                    </p>
+                        <p className="text-white cursor-pointer p-1 sub hover:scale-105 transition duration-300">
+                            Garajınızı ya da işyerinizi, her türlü hava şartlarından, hırsızlık ve zarar verme amaçlı
+                            yapılan fiziksel
+                            etkilerden korumak için, üstün güvenlik sistemiyle donatılmış, uzun ömürlü, rollmina Garaj
+                            Kapısı Sistemlerini
+                            güvenle binalarınıza uygulayabilirsiniz.
+                        </p>
                     </div>
                 </div>
                 <div className="shadow" style={{display:"flex",justifyContent:'center',background: 'lightblue'}}>
--- src/components/BusinessCard.jsx
+++ src/components/BusinessCard.jsx
@@ -3,14 +3,34 @@ import './BusinessCard.css';
 
 const BusinessCard = () => {
     return (
-        <div className="business-card">
-            <div className="card-content">
-                <h1 className="name">Volkan Kuru</h1>
-                <p className="title">Otomatik Kapı Sistemleri Uzmanı</p>
-                <p className="company">Çorlu Otomatik Kapı Sistemleri</p>
-                <div className="contact-info">
-                    <p><i className="fas fa-phone"></i> +90 542 275 55 59</p>
-                    <p><i className="fas fa-map-marker-alt"></i> Çorlu, Tekirdağ</p>
+        <div className="">
+            <div className="business-card flex justify-center">
+                <div className="card-content">
+                    <section
+                        className="contact-info text-xl font flex flex-wrap flex-col items-center justify-center text-center">
+                        <h2 className="font-bold text-2xl mb-4 name border-bcrimson">Çorlu Tekirdağ Bölgesi Otomatik Kapı ve Fabrika
+                            Kapıları</h2>
+                        <p className="text-2xl">
+                            Çorlu Tekirdağ bölgesinde otomatik kapı ve fabrika kapıları konusunda uzmanız.
+                            Müşterilerimize en yüksek kalitede otomatik geçiş sistemleri sunmaktayız.
+                            Ürün yelpazemiz, endüstriyel tesislerde, ticari alanlarda ve konutlarda güvenliği ve
+                            işlevselliği artırmak için tasarlanmış çeşitli otomatik kapıları
+                            ve geçiş sistemlerini içermektedir. Modern teknolojiyle donatılmış otomatik kapılarımız,
+                            enerji verimliliği ve dayanıklılık sağlarken, estetik açıdan da
+                            her türlü mimari yapıya uyum sağlar. Profesyonel ekibimiz, ihtiyaçlarınıza en uygun
+                            çözümleri sunmak için her zaman hazırdır. Bizimle çalışarak,
+                            güvenli, pratik ve şık kapı sistemlerine sahip olabilirsiniz.
+                        </p>
+                    </section>
+                    <p className=" text-center">Çorlu Otomatik Kapı Sistemleri</p>
+                    <h1 className="text-center hover:scale-105 transition duration-300 cursor-pointer">Volkan Kuru</h1>
+                    <div className="text-xl font contact-info flex flex-wrap flex-row items-center justify-center">
+                        <div>Çağrı İçin Lütfen Tıklayınız.</div>
+                        <a href="tel:+905422755559" className="bg-amber-100 flex items-center">
+                            &#9742; +90 542 275 55 59
+                        </a>
+                        <p>&#x1F4CD; Çorlu, Tekirdağ</p>
+                    </div>
                 </div>
             </div>
         </div>
